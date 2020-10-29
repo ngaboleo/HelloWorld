620c67db1ef6eaa92b1ef377a31b64418f6da942
ngaboleo <ngaboleonard02@gmail.com>
2020-10-29 13:04:01 +0200

    Thank Line added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World From Java");
+        System.out.println("Thank From Java");
     }
 }
