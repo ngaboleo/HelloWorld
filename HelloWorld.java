0c797c213da1b25aaf626c6ac83a276f736a26fc
ngaboleo <ngaboleonard02@gmail.com>
2020-10-29 13:10:25 +0200

    Amazing line added

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World From Java");
         System.out.println("Thank From Java");
+        System.out.println("Amazing");
     }
 }
